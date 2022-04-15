6aa15751cd4b78f4bd28fdf7bd483ae9b2d32c94
Premrathod <50791267+Premrathod@users.noreply.github.com>
2022-04-15 18:19:56 +0530

    added more_info page and imroved UI

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('about/',views.about, name='about'),
+    path('more_info/',views.more_info, name='more_info'),
     path('signin/',views.signin, name='signin'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('annotate/', views.upload_annotate, name='annotate'),
--- pages/views.py
+++ pages/views.py
@@ -9,6 +9,8 @@ def home(request):
     return render(request, "home.html", {})
 def about(request):
     return render(request, "about.html", {})
+def more_info(request):
+    return render(request, "more_info.html", {})
 def signin(request):
     return render(request, "signin.html", {})
 
